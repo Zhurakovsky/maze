af8b0922e44c370fc28cc6f72e1a2f8e381f204f
Yurii Zhurakovskyi <yurii.zhurakovskyi@1000eyes.org>
2021-11-14 00:44:01 +0200

    Solved maze

--- main.cpp
+++ main.cpp
@@ -6,18 +6,29 @@
 
 using namespace std;
 
+namespace
+{
+    /* ЭТО ЕДИНАЯ ТОЧКА, где мы ЗАДАЕМ ЖЕЛАЕМЫЕ размеры лабиринта.
+    Нужно помнить, что чем больше размеры, тем сложнее (реже) будет получить проходимый лабиринт, так как мы заполняем его случайными числами  */
+    const int ROWS = 3; 
+    const int COLS = 3;
+};
+
+
 enum maze_result_t
 {
     SOLVED = 0,
     NOT_SOLVED
-} ;
+};
 
-struct Cell_t
+
+struct Cell
 {
     int val;
-    int row;
-    int col;
+    int row_index; // Индекс ряда    от 0 до [rows-1]
+    int col_index; // Индекс колонки от 0 до [cols-1]
 };
+
 
 void set_entrance_and_exit(int** maze, int rows, int cols)
 {
@@ -28,6 +39,7 @@ void set_entrance_and_exit(int** maze, int rows, int cols)
     maze[rows-1][cols-1] = 3;
 }
 
+
 void fill_maze_by_random_numbers(int** maze, int rows, int cols)
 {
     for (auto row = 0; row < 3; row++)
@@ -37,10 +49,8 @@ void fill_maze_by_random_numbers(int** maze, int rows, int cols)
             maze[row][col] = rand() % 2; // fill cell by 0 or 1
         }
     }
-
-
-
 }
+
 
 void print_maze(int** maze, int rows, int cols)
 {
@@ -55,6 +65,51 @@ void print_maze(int** maze, int rows, int cols)
 }
 
 
+int check_top_neighbor(int** maze, int row, int col)
+{
+    /* Проверим допустимые границы */
+    if (row == 0)
+    {
+        return 0;
+    }
+
+    return maze[row-1][col];
+}
+
+
+int check_left_neighbor(int** maze, int row, int col)
+{
+    if (col == 0)
+    {
+        return 0;
+    }
+
+    return maze[row][col-1];
+}
+
+
+int check_bottom_neighbor(int** maze, int row, int col)
+{
+    if (row == (ROWS - 1))
+    {
+        return 0;
+    }
+
+    return maze[row + 1][col];
+}
+
+
+int check_right_neighbor(int** maze, int row, int col)
+{
+    if (col == (COLS - 1))
+    {
+        return 0;
+    }
+
+    return maze[row][col+1];
+}
+
+
 maze_result_t check_maze(int** maze, int rows, int cols)
 {
     maze_result_t result = NOT_SOLVED;
@@ -62,7 +117,160 @@ maze_result_t check_maze(int** maze, int rows, int cols)
     /* Для проверки напечатаем лабиринт */
     print_maze(maze, rows, cols);
 
-    //  change value
+    /* Создаем очередь, в которую будем помещать валидные (непосещенные клетки со значением 1) объекты-клетки */
+    std::queue<Cell> cells{}; // очередь пока пуста
+
+    /* Левая верхнаяя клетка всегда валидна, поскольку мы ей принудительно ставим 1. И посещенных клеток у нас пока что нету. Так что создадим наш объект первой клетки и поместим его в очередь */
+
+    Cell cell;
+    cell.val = maze[0][0]; // Это поле по нашим условиям всегда вход и равно 1
+    cell.row_index = 0;    // Первый ряд
+    cell.col_index = 0;    // Первая колонка
+    
+    cells.push(cell);
+
+    /*
+    Теперь у нас есть очередь, в которой есть первая клетка.
+    Теперь в цикле будем:
+      - извлекать элементы из очереди
+      - помечать в матрице нашего лабиринта клетку извлеченного элемента как посещенную (9)
+      - проверять соседей этой клетки:
+            -- Если сосед является выходом из лабиринта - завершаем выполнение и цикла и программы. 
+            -- Если сосед не является выходом, но подходящий (1) - создадим объект клетки соседа и поместим в ту же очередь.
+
+      В итоге - мы или найдем выход, или кончатся элементы в очереди, что означает, что лабиринт не имеет решения. Значит возвращаем соответствующий результат и завершаем программу.
+    */
+
+   while(!cells.empty())
+   {
+       /* Создали элемент из первого элемента очереди */
+       Cell extracted_front_cell = cells.front();   
+       
+       /* Проверили, что данные нормально скопировались. */
+       cout << "Extracted data: val = " << extracted_front_cell.val
+        << ", row_index = " << extracted_front_cell.row_index 
+        << ", col_index = " << extracted_front_cell.col_index << endl; 
+
+        /* Не забыли удалить первый элемент, чтобы очередь продвинулась. */
+        cells.pop();
+
+        /* Еесли извлеченная клетка выход - успешно выходим из функции и из программы */
+        if (extracted_front_cell.val == 3)
+        {
+            result = SOLVED;
+            return result;
+        }
+
+        /* Пометим клетку в нашем лабиринте как посещенную (9). Не забываем, что помечаем клеточку в исходном лабиринте. Но данные о посещенной клетке - это данные из объекта, извлеченного из очереди. Тут важно не запутаться. */
+        maze[extracted_front_cell.row_index][extracted_front_cell.col_index] = 9;
+
+        /* Для уверенности можем напечатать наш лабиринт с внесенными изменениями */
+        print_maze(maze, rows, cols);
+
+        /* Проверим верхнего соседа. Есть он или нет - проверит функция, которая вернет значение верхней (если она есть) клетки в лабиринте. Если это выход, значит выходим из функции и из программы. В вызываемую функцию передадим координаты нашей текущей клетки. Которую извлекли и с которой работаем. */
+
+        int top_val = check_top_neighbor(maze,
+                                        extracted_front_cell.row_index,
+                                        extracted_front_cell.col_index);
+        switch(top_val)
+        {
+            case 3:
+            result = SOLVED;
+            return result;
+            // break;  /* break тут ставить по правилам нужно, но бессмыссленно. Мы вышли из функции и дальше программа не пойдет */
+
+            case 1:
+            /* Наш случай. Нужно из верхнего соседа создать объект клетки и поместить в очередь. Насчет выхода за границы массива можно не переживать. Функция это проверила */
+            Cell cell;
+            cell.val = maze[extracted_front_cell.row_index - 1][extracted_front_cell.col_index];
+            cell.row_index = (extracted_front_cell.row_index - 1);
+            cell.col_index = extracted_front_cell.col_index;
+            
+            cells.push(cell);
+            break;
+
+            default:
+            /* Тут любые другие значения клеток лабиринта (0 или 9). Не делаем с ними ничего */
+            break;
+        }
+
+        /* 
+        Дальше по аналогии. Проверяем левого, нижнего и правого соседей. Тут я проверяю соседей против часовой стрелки. Можно проверять в любом порядке.
+        
+        ВАЖНО: тут есть полно возможностей оптимизировать и сокращать количество кода. Я не делаю этого просто для наглядности.
+        */
+
+        int left_val = check_left_neighbor(maze,
+                                        extracted_front_cell.row_index,
+                                        extracted_front_cell.col_index);
+        switch(left_val)
+        {
+            case 3:
+                result = SOLVED;
+                return result;
+                // break;
+
+            case 1:
+                Cell cell;
+                cell.val = maze[extracted_front_cell.row_index][extracted_front_cell.col_index - 1];
+                cell.row_index = (extracted_front_cell.row_index);
+                cell.col_index = (extracted_front_cell.col_index - 1);
+                
+                cells.push(cell);
+                break;
+
+            default:
+                break;
+        }
+
+        int bottom_val = check_bottom_neighbor(maze,
+                                        extracted_front_cell.row_index,
+                                        extracted_front_cell.col_index);
+        switch(bottom_val)
+        {
+            case 3:
+                result = SOLVED;
+                return result;
+                //break;
+
+            case 1:  
+                Cell cell;
+                cell.val = maze[extracted_front_cell.row_index + 1][extracted_front_cell.col_index];
+                cell.row_index = (extracted_front_cell.row_index + 1);
+                cell.col_index = (extracted_front_cell.col_index);
+                
+                cells.push(cell);
+                break;
+
+            default:
+                break;
+        }
+
+        int right_val = check_right_neighbor(maze,
+                                        extracted_front_cell.row_index,
+                                        extracted_front_cell.col_index);
+        switch(right_val)
+        {
+            case 3:
+                result = SOLVED;
+                return result;
+                // break;
+
+            case 1:
+                Cell cell;
+                cell.val = maze[extracted_front_cell.row_index][extracted_front_cell.col_index + 1];
+                cell.row_index = (extracted_front_cell.row_index);
+                cell.col_index = (extracted_front_cell.col_index + 1);
+                
+                cells.push(cell);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /* В принципе - все. Мы или нашли выход и уже успешно вышли из функции где-то раньше, или пришли сюда. Это значит, что лабиринт не имеет решения и мы возвращаем значение NOT_SOLVED, как его в самом начале и установили */
 
     return result;
 }
@@ -74,27 +282,37 @@ int main()
     srand (time(NULL));
     
     // Создадим лабиринт и выделим для него память
-    int rows = 3; 
-    int cols = 3;
     int** maze;
-
-    maze = new int*[rows];
-    for (size_t row = 0; row < rows; row++)
+    maze = new int*[ROWS];
+    for (size_t row = 0; row < ROWS; row++)
     {
-        maze[row] = new int[cols];
+        maze[row] = new int[COLS];
     }
 
     /* Заполним лабиринт случайными 0 и 1 */
-    fill_maze_by_random_numbers(maze, rows, cols);
+    fill_maze_by_random_numbers(maze, ROWS, COLS);
     
     /* Oтметим обязательные точки входа и выхода */
-    set_entrance_and_exit(maze, rows, cols);
+    set_entrance_and_exit(maze, ROWS, COLS);
 
-    /* Передадим лабиринт в функцию проверки */
-    check_maze(maze, rows, cols);
-    
+    /* Проверим, имеет ли лабиринт решение и напишем соответствующий ответ. */
+    if (check_maze(maze, ROWS, COLS) == SOLVED)
+    {
+        cout << "MAZE SOLVED!" << endl;
+    }
+    else
+    {
+        cout << "MAZE NOT SOLVED!" << endl;         
+    }
+
+    /* Мы работали с ручным выделением памяти. Не забываем очистить ее после использования */
+    for (size_t row = 0; row < ROWS; row++)
+    {
+        delete [] maze[row];
+    }
+
+    delete [] maze;
+    maze = nullptr;
+
     return 0;
 }
-
-// Алгоритм Дейкстры
-// Как передать двумерный массив в функцию
