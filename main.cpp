23545cbbb0b73601249fb8cc1b1ed88e47f02992
Yurii Zhurakovskyi <yurii.zhurakovskyi@1000eyes.org>
2021-11-13 22:29:47 +0200

    Basic maze fill and print

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <queue>
 
+#include <cstdlib>     /* srand, rand */
+#include <ctime>       /* time */
+
 using namespace std;
 
 enum maze_result_t
@@ -16,35 +19,80 @@ struct Cell_t
     int col;
 };
 
+void set_entrance_and_exit(int** maze, int rows, int cols)
+{
+    /* Пусть левая верхняя клетка будет всегда 1. Это будет наш вход. */
+    maze[0][0] = 1;
 
-maze_result_t check_maze(int maze[3][3], int rows, int cols)
+    /* Пусть правая нижняя клетка будет всегда 3. Это будет наш выход. */
+    maze[rows-1][cols-1] = 3;
+}
+
+void fill_maze_by_random_numbers(int** maze, int rows, int cols)
+{
+    for (auto row = 0; row < 3; row++)
+    {
+        for (auto col = 0; col < 3; col++)
+        {
+            maze[row][col] = rand() % 2; // fill cell by 0 or 1
+        }
+    }
+
+
+
+}
+
+void print_maze(int** maze, int rows, int cols)
+{
+    for (auto row = 0; row < rows; row++)
+    {
+        for (auto col = 0; col < cols; col++)
+        {
+            cout << maze[row][col] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+maze_result_t check_maze(int** maze, int rows, int cols)
 {
     maze_result_t result = NOT_SOLVED;
 
-    //print array
+    /* Для проверки напечатаем лабиринт */
+    print_maze(maze, rows, cols);
 
     //  change value
 
     return result;
 }
 
-void hello()
-{
-    cout << "Hello" << endl;
-}
 
 int main()
 {
+    /* initialize random seed: */
+    srand (time(NULL));
+    
+    // Создадим лабиринт и выделим для него память
+    int rows = 3; 
+    int cols = 3;
+    int** maze;
 
-    //hello();
+    maze = new int*[rows];
+    for (size_t row = 0; row < rows; row++)
+    {
+        maze[row] = new int[cols];
+    }
 
-    int  maze[3][3] = {1,1,1,1,1,1,1,1,1}; 
+    /* Заполним лабиринт случайными 0 и 1 */
+    fill_maze_by_random_numbers(maze, rows, cols);
+    
+    /* Oтметим обязательные точки входа и выхода */
+    set_entrance_and_exit(maze, rows, cols);
 
-    // Создадим лабиринт
-    // Передадим лабиринт в функцию
-
-    check_maze(maze, 3, 3);
-    //cout << "Hello World" << endl;
+    /* Передадим лабиринт в функцию проверки */
+    check_maze(maze, rows, cols);
+    
     return 0;
 }
 
